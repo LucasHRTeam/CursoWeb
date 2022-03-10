9149f31dae3bd614deb0c6e3c7684afc4f7a3f67
Lucas Santos Da Hora <89528999+LucasHRTeam@users.noreply.github.com>
2022-03-10 08:41:10 -0400

    Update ArrayEObjetos.js

--- Exercicios/JavaScript/ArrayEObjetos.js
+++ Exercicios/JavaScript/ArrayEObjetos.js
@@ -1,1 +1,13 @@
+// a exemplo da função .forEach dos objetos,
+// crie uma nova função com essa mesma finalidade 
+// usando herança.
 
+Array.prototype.newForEach = function(callback) {
+   for (let i = 0; i < this.length; i++) {
+     callback(this[i], i, this)
+  }
+}
+
+
+const idUser = ["231019", "575942", "34564"]
+   .newForEach(item => console.log(item))
