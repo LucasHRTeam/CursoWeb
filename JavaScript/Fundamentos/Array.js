eedb2b5abe1116e6f768492ca836dd7fe13de098
Lucas Santos Da Hora <89528999+LucasHRTeam@users.noreply.github.com>
2022-03-10 00:04:56 -0400

    Create Array.js

---
+++ JavaScript/Fundamentos/Array.js
@@ -0,0 +1,16 @@
+// As estruturas denominadas arrays são usadas para
+// atribuir mais de um elemento em uma mesma variável 
+// seu type === 'object'. Sua estrutura é organizada 
+// por índices, onde cada um representa um elemento
+
+
+const listCodeCountry = ["BRL", "USD", "EUR"];
+
+// console.log(listCodeCountry[0]) ~> BRL
+
+// funções importantes:
+
+Array.sort();
+// reorganiza os elementos do array em ordem crescente 
+// caso os elementos sejam strings ele organiza em
+// ordem alfabética 
