48b4ffc8a11470f67b6d94d04a7d8d352ffd8a93
Lucas Santos Da Hora <89528999+LucasHRTeam@users.noreply.github.com>
2022-03-10 00:09:49 -0400

    Update Array.js

--- JavaScript/Fundamentos/Array.js
+++ JavaScript/Fundamentos/Array.js
@@ -14,3 +14,22 @@ Array.sort();
 // reorganiza os elementos do array em ordem crescente 
 // caso os elementos sejam strings ele organiza em
 // ordem alfabética 
+
+
+Array.push(value);
+// este adiciona um novo elemento na última posição.
+// usado para inserir novas informações
+
+
+Array.unshift(value);
+// este faz a mesma tarefa do .push(), mas a diferença
+// é que este adiciona o novo elemento no início do
+// array, já o outro adiciona no final.
+
+
+Array.splice(um, dois, três);
+// esta apaga algum elemento do array, especificado
+// pelo "um", que é o índice dele e tem o
+// type === "number". O "dois" é a quantidade de índices
+// que serão apagados a partir do "um".
+
